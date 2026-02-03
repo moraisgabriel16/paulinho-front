d4ca603553041a5fbdec47333d83bab5cfbbaacd
Gabriel Morais <gabriel@example.com>
2026-02-03 13:57:46 -0300

    fix url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://paulinho-back.vercel.app/api';
 
 const apiClient: AxiosInstance = axios.create({
   baseURL: API_URL,
